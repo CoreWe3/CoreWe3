f3316ee5e64b57367fc9b37a7b569a70f9d0112c
emak <emak.ryo@gmail.com>
2014-10-09 12:59:06 +0900

    create folder core

---
+++ core/core.vhd
@@ -0,0 +1,73 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+library UNISIM;
+use UNISIM.VComponents.all;
+
+entity core is
+  
+  port (
+    MCLK1  : in    std_logic;
+    RS_RX  : in    std_logic;
+    RS_TX  : out   std_logic;
+    ZD     : inout std_logic_vector(31 downto 0);
+    ZA     : out   std_logic_vector(19 downto 0);
+    XE1    : out   std_logic;
+    E2A    : out   std_logic;
+    XE3    : out   std_logic;
+    XZBE   : out   std_logic_vector(3 downto 0);
+    XGA    : out   std_logic;
+    XWA    : out   std_logic;
+    XZCKE  : out   std_logic;
+    ZCLKMA : out   std_logic_vector(1 downto 0);
+    ADVA   : out   std_logic;
+    XFT    : out   std_logic;
+    XLBO   : out   std_logic;
+    ZZA    : out   std_logic);
+end core;
+
+architecture arch_core of core is
+  component core_main
+    port (
+      clk   : in    std_logic;
+      RS_TX : out   std_logic;
+      RS_RX : in    std_logic;
+      ZD    : inout std_logic_vector(31 downto 0);
+      ZA    : out   std_logic_vector(19 downto 0);
+      XWA   : out   std_logic);
+  end component;
+
+  signal iclk : std_logic;
+  signal clk : std_logic;
+begin  -- arch_core
+
+  ib : IBUFG port map (
+    i => MCLK1,
+    o => iclk);
+  bg : BUFG port map (
+    i => iclk,
+    o => clk);
+
+  main : core_main port map (
+    clk   => clk,
+    RS_TX => RS_TX,
+    RS_RX => RS_RX,
+    ZD    => ZD,
+    ZA    => ZA,
+    XWA   => XWA);
+
+  XE1 <= '0';
+  E2A <= '1';
+  XE3 <= '0';
+  XZBE <= "0000";
+  XGA <= '0';
+  XZCKE <= '0';
+  ZCLKMA(0) <= clk;
+  ZCLKMA(1) <= clk;
+  ADVA <= '0';
+  XFT <= '1';
+  XLBO <= '1';
+  ZZA <= '0';
+
+end arch_core;
