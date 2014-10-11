ccfce8a0d25d0208aa88c66626e111cf50f24299
wataru <you@example.com>
2014-10-11 23:30:00 +0900

    preprosesser of min-tr.ml

---
+++ compiler/raytracer/preprocess.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import re
+import sys
+
+if __name__ == "__main__":
+    f = open(sys.argv[1])
+    text = f.read()
+    f.close()
+    text = re.compile(r'\(\*NOMINCAML.*\*\)').sub('', text)
+    text = re.compile(r'\(\*MINCAML\*\).*').sub('', text)
+    text = re.compile(r' \* 4 ').sub(' lsl 2 ', text)
+    text = re.compile(r' \/ 2').sub(' lsr 1', text)
+    text = re.compile(r';\n[\s\t]*\)', re.M).sub(')', text)    
+    print(text)
