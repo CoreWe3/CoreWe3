dfd51ee626b96acbd5f7ad5c3fbbcce88d4924d9
emak <emak.ryo@gmail.com>
2014-10-15 13:54:48 +0900

    add comment to fib_rec.s

--- file/fib_rec.s
+++ file/fib_rec.s
@@ -4,11 +4,11 @@ JSUB	:fib
 ST	r1	r0	65535
 BEQ	r0	r0	:main
 :fib				#fib n
-BEQ	r1	r0	:base	#base
-ADDI	r2	r0	1
-BEQ	r1	r2	:base	#base
-SUB	r1	r1	r2	#step
-SUB	r3	r1	r2
+BEQ	r1	r0	:base	#if n == 0
+ADDI	r2	r0	1	#r2 = 1
+BEQ	r1	r2	:base	#if n == 1
+SUB	r1	r1	r2	#else r1 = n-1
+SUB	r3	r1	r2	#r3 = n-2
 PUSH	r3
 JSUB	:fib			#fib n-1
 POP	r2
