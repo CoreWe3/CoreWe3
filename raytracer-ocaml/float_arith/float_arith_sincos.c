154ad8bc2a398d0a82bce2b8a0495d285fd5eaa5
wataru <wtr816@gmail.com>
2014-10-21 04:43:40 +0900

    change register usage in fsin

--- raytracer-ocaml/float_arith/float_arith_sincos.c
+++ raytracer-ocaml/float_arith/float_arith_sincos.c
@@ -77,76 +77,76 @@ uint32_t kernel_sin(uint32_t a){
 uint32_t fsin(uint32_t a){
     uint32_t r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15;
     uint32_t s1, s2, s3, s4;
-    r1 = a;
+    r2 = a;
     
-    r2 = r1 >> 31;
-    r1 = r1 << 1;
-    r1 = r1 >> 1;
-    s1 = r2;
-    r1 = reduction(r1);
-    r2 = s1;
+    r3 = r2 >> 31;
+    r2 = r2 << 1;
+    r2 = r2 >> 1;
+    s1 = r3;
+    r2 = reduction(r2);
+    r3 = s1;
     
     //r15 = 0x490fdb(man of PI)
-    r3 = 16;
+    r4 = 16;
     r15 = 0x49;
-    r15 = r15 << r3;
-    r4 = 0x0fdb;
-    r15 = r15 | r4;
+    r15 = r15 << r4;
+    r5 = 0x0fdb;
+    r15 = r15 | r5;
     //r14 = 0x40490fdb(PI)
-    r3 = 23;
+    r4 = 23;
     r14 = 0x80;
-    r14 = r14 << r3;
+    r14 = r14 << r4;
     r14 = r14 | r15;
     
-    if (r1 < r14 /*r1 >= r14*/) {} else {
-	s1 = r2;
+    if (r2 < r14 /*r2 >= r14*/) {} else {
+	s1 = r3;
 	s2 = r15;
 	s3 = r14;
-	r2 = r14;
-	r1 = fsub(r1, r2);
+	r3 = r14;
+	r2 = fsub(r2, r3);
 	r14 = s3;
 	r15 = s2;
-	r2 = s1;
-	if(r2 != 0 /*r2 == 0*/) {
-	    r2 = 1;
+	r3 = s1;
+	if(r3 != 0 /*r3 == 0*/) {
+	    r3 = 1;
 	} else {
-	    r2 = 0;
+	    r3 = 0;
 	}
     }
 
     //r13 = 0x3fc90fdb(PI/2)    
-    r3 = 23;
+    r4 = 23;
     r13 = 0x7f;
     r13 = r13 << r3;
     r13 = r13 | r15;    
-    if(r1 < r13 /*r1 >= r13*/) {} else {
-	s1 = r2;
+    if(r2 < r13 /*r2 >= r13*/) {} else {
+	s1 = r3;
 	s2 = r15;
 	s3 = r13;
-	r2 = r1;
-	r1 = r14;
-	r1 = fsub(r1, r2);
+	r3 = r2;
+	r2 = r14;
+	r2 = fsub(r2, r3);
 	r13 = s3;
 	r15 = s2;
     }
 
     //r12 = 0x3f490fdb(PI/4)
-    r3 = 23;
+    r4 = 23;
     r12 = 0x7e;
-    r12 = r12 << r3;
+    r12 = r12 << r4;
     r12 = r12 | r15;
-    if(r12 < r1 /*r1 <= r13*/) {
-	r2 = r1;
-	r1 = r13;
-	r1 = fsub(r1, r2);
-	r1 = kernel_cos(r1);
+    if(r12 < r2 /*r2 <= r13*/) {
+	r3 = r2;
+	r2 = r13;
+	r2 = fsub(r2, r3);
+	r2 = kernel_cos(r2);
     } else {
-	r1 = kernel_sin(r1);
+	r2 = kernel_sin(r2);
     }
 
-    r3 = 31;
-    r2 = s1;
-    r2 = r2 << r3;
-    r1 = r2 | r1;
-    return r1;
+    r4 = 31;
+    r3 = s1;
+    r3 = r3 << r3;
+    r2 = r3 | r2;
+    return r2;
 }
