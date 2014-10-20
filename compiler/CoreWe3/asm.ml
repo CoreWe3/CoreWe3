bfbb13ad9d0da6e1ae41571331d7ec071b1d9aee
Osamu Ishimura <paken_59_tek@live.jp>
2014-10-21 02:11:32 +0900

    work on compiler

---
+++ compiler/CoreWe3/asm.ml
@@ -0,0 +1,96 @@
+(* CoreWe3 assembly with a few virtual instructions *)
+
+type id_or_imm = V of Id.t | C of int
+type t = (* 命令の列 *)
+  | Ans of exp
+  | Let of (Id.t * Type.t) * exp * t
+and exp = (* 一つ一つの命令に対応する式 *)
+  | Nop
+  | Li of int
+(*| FLi of Id.l *)
+  | SetL of Id.l
+  | Mr of Id.t
+  | Neg of Id.t
+  | Add of Id.t * id_or_imm
+  | Sub of Id.t * id_or_imm
+  | Slw of Id.t * id_or_imm
+  | Srw of Id.t * id_or_imm
+(*| Lwz of Id.t * id_or_imm
+  | Stw of Id.t * Id.t * id_or_imm*)
+(*| FMr of Id.t 
+  | FNeg of Id.t
+  | FAdd of Id.t * Id.t
+  | FSub of Id.t * Id.t
+  | FMul of Id.t * Id.t
+  | FDiv of Id.t * Id.t 
+  | Lfd of Id.t * id_or_imm
+  | Stfd of Id.t * Id.t * id_or_imm
+  | Comment of string*)
+  (* virtual instructions *)
+  | IfEq of Id.t * id_or_imm * t * t
+  | IfLE of Id.t * id_or_imm * t * t
+  | IfGE of Id.t * id_or_imm * t * t
+(*| IfFEq of Id.t * Id.t * t * t
+  | IfFLE of Id.t * Id.t * t * t *)
+  (* closure address, integer arguments, and float arguments *)
+  | CallCls of Id.t * Id.t list
+  | CallDir of Id.l * Id.t list
+  | Save of Id.t * Id.t (* レジスタ変数の値をスタック変数へ保存 *)
+  | Restore of Id.t (* スタック変数から値を復元 *)
+type fundef =
+    { name : Id.l; args : Id.t list; body : t; ret : Type.t }
+(* プログラム全体 = 浮動小数点数テーブル + トップレベル関数 + メインの式 *)
+type prog = Prog of (Id.l * float) list * fundef list * t
+
+(* shorthand of Let for float *)
+(* fletd : Id.t * exp * t -> t *)
+let fletd (x, e1, e2) = Let ((x, Type.Float), e1, e2)
+(* shorthand of Let for unit *)
+(* seq : exp * t -> t *)
+let seq (e1, e2) = Let ((Id.gentmp Type.Unit, Type.Unit), e1, e2)
+
+let regs = [| "%r1"; "%r2"; "%r3"; "%r4"; "%r5"; "%r6"; "%r7"; "%r8"; "%r9"; "%r10"; 
+  "%r11"; "%r12"; "%r13"; "%r14"; "%r15";|]
+
+(* let regs = Array.init 27 (fun i -> Printf.sprintf "_R_%d" i) *)
+let allregs = Array.to_list regs
+let reg_cl = regs.(Array.length regs - 1) (* closure address *)
+let reg_sw = regs.(Array.length regs - 2) (* temporary for swap *)
+
+(* is_reg : Id.t -> bool *)
+let is_reg x = x.[0] = '%'
+
+(* remove_and_uniq : S.t -> Id.t list -> Id.t list *)
+let rec remove_and_uniq xs = function 
+  | [] -> []
+  | x :: ys when S.mem x xs -> remove_and_uniq xs ys
+  | x :: ys -> x :: remove_and_uniq (S.add x xs) ys
+
+(* free variables in the order of use (for spilling) *)
+(* fv_id_or_imm : id_or_imm -> Id.t list *)
+let fv_id_or_imm = function V (x) -> [x] | _ -> []
+(* fv_exp : Id.t list -> t -> S.t list *)
+let rec fv_exp = function
+  | Nop | Li (_) | SetL (_) | Restore(_) -> []
+  | Mr (x) | Neg (x) | Save (x, _) -> [x]
+  | Add (x, y') | Sub (x, y') | Slw (x, y') | Srw (x, y') -> 
+      x :: fv_id_or_imm y'
+  | IfEq (x, y', e1, e2) | IfLE (x, y', e1, e2) | IfGE (x, y', e1, e2) -> 
+      x :: fv_id_or_imm y' @ remove_and_uniq S.empty (fv e1 @ fv e2)
+  | CallCls (x, ys) -> x :: ys
+  | CallDir (_, ys) -> ys
+and fv = function 
+  | Ans (exp) -> fv_exp exp
+  | Let ((x, t), exp, e) ->
+      fv_exp exp @ remove_and_uniq (S.singleton x) (fv e)
+
+(* fv : t -> Id.t list *)
+let fv e = remove_and_uniq S.empty (fv e)
+
+(* concat : t -> Id.t * Type.t -> t -> t *)
+let rec concat e1 xt e2 = match e1 with
+  | Ans (exp) -> Let (xt, exp, e2)
+  | Let (yt, exp, e1') -> Let (yt, exp, concat e1' xt e2)
+
+(* align : int -> int *)
+let align i = if i mod 8 = 0 then i else i + 4
---
+++ compiler/CoreWe3/asm.mli
@@ -0,0 +1,40 @@
+type id_or_imm = V of Id.t | C of int
+type t = 
+  | Ans of exp
+  | Let of (Id.t * Type.t) * exp * t
+and exp = 
+  | Nop
+  | Li of int
+  | SetL of Id.l
+  | Mr of Id.t
+  | Neg of Id.t
+  | Add of Id.t * id_or_imm
+  | Sub of Id.t * id_or_imm
+  | Slw of Id.t * id_or_imm
+  | Srw of Id.t * id_or_imm
+  (* virtual instructions *)
+  | IfEq of Id.t * id_or_imm * t * t
+  | IfLE of Id.t * id_or_imm * t * t
+  | IfGE of Id.t * id_or_imm * t * t (* for simm *)
+  (* closure address, integer arguments, and float arguments *)
+  | CallCls of Id.t * Id.t list
+  | CallDir of Id.l * Id.t list
+  | Save of Id.t * Id.t (* レジスタ変数の値をスタック変数へ保存 *)
+  | Restore of Id.t (* スタック変数から値を復元 *)
+type fundef =
+    { name : Id.l; args : Id.t list; body : t; ret : Type.t }
+type prog = Prog of (Id.l * float) list * fundef list * t
+
+val fletd : Id.t * exp * t -> t (* shorthand of Let for float *)
+val seq : exp * t -> t (* shorthand of Let for unit *)
+
+val regs : Id.t array
+val allregs : Id.t list
+val reg_cl : Id.t
+val reg_sw : Id.t
+val is_reg : Id.t -> bool
+
+val fv : t -> Id.t list
+val concat : t -> Id.t * Type.t -> t -> t
+
+val align : int -> int
