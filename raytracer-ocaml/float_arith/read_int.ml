b3dcf2c4c819b4fd12de1f08e72a04848979a665
mori0501 <td_hk_m0501@yahoo.co.jp>
2014-11-10 00:53:57 +0900

    add read_float.ml

--- raytracer-ocaml/float_arith/read_int.ml
+++ raytracer-ocaml/float_arith/read_int.ml
@@ -1,6 +1,6 @@
 
 let read_int () =
-  let rec read_int_sub () ret flag =
+  let rec read_int_sub ret flag =
     let ch = read_char () in
     if flag = 1 then
       if ch = 32 || ch = 9 || ch = 10 || ch = 13 
@@ -8,13 +8,12 @@ let read_int () =
       else 
 	let temp = ch - 48
 	in
-	read_int_sub () ((ret lsl 1) + (ret lsl 3) + temp) 1
+	read_int_sub ((ret lsl 1) + (ret lsl 3) + temp) 1
     else 
       if ch = 32 || ch = 9 || ch = 10 || ch = 13 
-      then read_int_sub () 0 0
+      then read_int_sub 0 0
       else 
 	let temp = ch - 48
 	in
 	read_int_sub () ((ret lsl 1) + (ret lsl 3) + temp) 1
-  in read_int_sub () 0 0
-    in read_int ()
+  in read_int_sub 0 0
