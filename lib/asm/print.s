91e4f37c992a6a1262d1cfeff936bf869e12ec06
wataru <wtr816@gmail.com>
2014-11-14 02:13:31 +0900

    raytracer initialize routine

--- lib/asm/print.s
+++ lib/asm/print.s
@@ -4,3 +4,12 @@
 :min_caml_print_int
 	JSUB	:min_caml_print_char
 	RET
+:min_caml_print_int32
+	ADDI	r4	r0	24	
+	ADDI	r5	r3	0
+:min_caml_print_int32_L1
+	SHR	r3	r5	r4
+	JSUB	:min_caml_print_char
+	ADDI	r4	r4	-8
+	BLE	r0	r4	:min_caml_print_int32_L1
+	RET
