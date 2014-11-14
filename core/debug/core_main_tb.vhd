b8de44ededcea6ebbb7fc83c1be9bfe3c284eb6f
emak <emak.ryo@gmail.com>
2014-11-14 20:12:12 +0900

    increase clock freq but not work

--- core/debug/core_main_tb.vhd
+++ core/debug/core_main_tb.vhd
@@ -1,25 +1,29 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
 
 entity core_main_tb is
 end core_main_tb;
 
 architecture arch_core_main_tb of core_main_tb is
-  constant wtime : std_logic_vector(15 downto 0) := x"000F";
+  constant CPB : integer := 10;
   
   component core_main
     generic (
-      CODE : string := "test.tbin";
-      wtime : std_logic_vector(15 downto 0) := wtime;
-      debug : boolean := false);
+      CODE       : string := "stack_test.tbin";
+      ADDR_WIDTH : integer := 8;
+      CLKR       : integer := 2;
+      memCPB     : integer := CPB;
+      debug      : boolean := false);
     port (
-      clk   : in    std_logic;
-      RS_TX : out   std_logic;
-      RS_RX : in    std_logic;
-      ZD    : inout std_logic_vector(31 downto 0);
-      ZA    : out   std_logic_vector(19 downto 0);
-      XWA   : out   std_logic);
+      sysclk : in    std_logic;
+      memclk : in    std_logic;
+      RS_TX  : out   std_logic;
+      RS_RX  : in    std_logic;
+      ZD     : inout std_logic_vector(31 downto 0);
+      ZA     : out   std_logic_vector(19 downto 0);
+      XWA    : out   std_logic);
   end component;
 
   component SRAM
@@ -32,7 +36,8 @@ architecture arch_core_main_tb of core_main_tb is
 
   component input_simulator
     generic (
-      wtime : std_logic_vector(15 downto 0) := wtime;
+      wtime : std_logic_vector(15 downto 0) :=
+      conv_std_logic_vector(CPB,16);
       INPUT_FILE : string := "test.bin");
     port (
       clk  : in  std_logic;
@@ -41,50 +46,60 @@ architecture arch_core_main_tb of core_main_tb is
 
   component output_simulator
     generic (
-      wtime : std_logic_vector(15 downto 0) := wtime;
+      wtime : std_logic_vector(15 downto 0) :=
+      conv_std_logic_vector(CPB,16);
       OUTPUT_FILE : string := "output");
     port (
       clk : std_logic;
       RS_TX : std_logic);
   end component;
   
-  signal clk   : std_logic; 
-  signal RS_TX : std_logic; 
-  signal RS_RX : std_logic; 
-  signal ZD    : std_logic_vector(31 downto 0); 
-  signal ZA    : std_logic_vector(19 downto 0); 
-  signal XWA   : std_logic;
+  signal sysclk : std_logic;
+  signal memclk : std_logic;
+  signal RS_TX  : std_logic;
+  signal RS_RX  : std_logic;
+  signal ZD     : std_logic_vector(31 downto 0);
+  signal ZA     : std_logic_vector(19 downto 0);
+  signal XWA    : std_logic;
 begin
 
   main : core_main port map (
-    clk   => clk   , 
-    RS_TX => RS_TX , 
-    RS_RX => RS_RX , 
-    ZD    => ZD    , 
-    ZA    => ZA    , 
-    XWA   => XWA   );
+    sysclk => sysclk,   
+    memclk => memclk,   
+    RS_TX  => RS_TX , 
+    RS_RX  => RS_RX , 
+    ZD     => ZD    , 
+    ZA     => ZA    , 
+    XWA    => XWA   );
 
   sram_sim : SRAM port map (
-    clk => clk,
-    ZD => ZD,
-    ZA => ZA,
+    clk => memclk,
+    ZD  => ZD,
+    ZA  => ZA,
     XWA => XWA);
 
   input_sim : input_simulator port map (
-    clk => clk,
+    clk   => memclk,
     RS_RX => RS_RX);
 
   output_sim : output_simulator port map (
-    clk => clk,
+    clk   => memclk,
     RS_TX => RS_TX);
     
   process
   begin
-    clk <= '0';
+    sysclk <= '0';
     wait for 1 ns;
-    clk <= '1';
+    sysclk <= '1';
     wait for 1 ns;
   end process;
 
-  
+  process
+  begin
+    memclk <= '0';
+    wait for 2 ns;
+    memclk <= '1';
+    wait for 2 ns;
+  end process;
+
 end arch_core_main_tb;
