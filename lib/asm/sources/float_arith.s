faba55ac5ec57c290b54e5a16fcd5eb79c96c8fa
wataru <wtr816@gmail.com>
2014-12-01 21:24:44 +0900

    fix fadd

--- lib/asm/sources/float_arith.s
+++ lib/asm/sources/float_arith.s
@@ -8,6 +8,14 @@
 	ADDI	r3	r4	r0
 	ADDI	r4	r7	r0
 :min_caml_fadd_L1
+	SHRI	r9	r3	31	#r9(10) = sig
+	SHRI	r10	r4	31
+	BEQ	r9	r10	:min_caml_fadd_abscmp
+	BLT	r5	r6	:min_caml_fadd_abscmp
+	BLT	r6	r5	:min_caml_fadd_abscmp
+	LDI	r3	0x0
+	RET
+:min_caml_fadd_abscmp
 	SHLI	r5	r3	1
 	SHRI	r5	r5	24
 	SHLI	r6	r4	1
@@ -17,13 +25,11 @@
 	SHRI	r7	r7	9
 	SHLI	r8	r4	9
 	SHRI	r8	r8	9
-	LDI	r9	0x800000
-	OR	r7	r9	r7
-	OR	r8	r9	r8
+	LDI	r12	0x800000
+	OR	r7	r12	r7
+	OR	r8	r12	r8
 	SHR	r8	r8	r6	#shift man of smaller one
-	SHRI	r9	r3	31	#r9(10) = sig
-	SHRI	r10	r4	31
-	BEQ	r9	r10	:min_caml_fadd_L2 	#r9 = man
+	BEQ	r9	r10	:min_caml_fadd_L2 	
 	SUB	r11	r7	r8
 	BEQ	r0	r0	:min_caml_fadd_L3
 :min_caml_fadd_L2
