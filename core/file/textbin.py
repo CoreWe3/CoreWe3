dbc294083af1abd7a42f8e009cf27a5dc01a3f52
emak <emak.ryo@gmail.com>
2014-12-08 16:43:19 +0900

    modify scripts

--- core/file/textbin.py
+++ core/file/textbin.py
@@ -2,24 +2,13 @@
 import struct
 import sys
 
-def binary(n):
-    a = n
-    s = ''
-    for i in range(8):
-        if a%2 == 0:
-            s = '0' + s
-        else:
-            s = '1' + s
-        a /= 2
-    return s
-
 l = 0
 while True :
     w = sys.stdin.read(4)
     s = ''
     if len(w) == 4:
         for t in reversed(struct.unpack('>4B',w)):
-            s += binary(t)
+            s += bin(t)[2:].zfill(8)
         print s
         l += 1
     else:
