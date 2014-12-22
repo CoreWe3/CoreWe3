07fc49842db8ca7bae7144413b05a2347ca54c16
Ryosuke Kamesawa <emak.ryo@gmail.com>
2014-12-22 09:34:46 +0900

    modify fib.s and scripts

--- core/file/conv.py
+++ core/file/conv.py
@@ -1,45 +1,74 @@
+class ParseError(Exception):
+    def __init__(self, s):
+        self.s = s
+
+    def __str__(self):
+        return str(self.s)
+
+
 class Instruction:
 
-    def __init__(self, s):
+    def __init__(self, s): # parse line
         self.opcode = None
-        self.operand = None
-        self.llabel = []
-        self.dlabel = []
+        self.operand_l = [] # list of operand
+        self.blabel_l = [] # list of branch label
+        self.ilabel_al = [] # assoc list of immediate label
         t = s.split()
-        if len(t) == 0:
-            raise Exception
-        elif len(t) == 1:
+        if len(t) == 0: # empty line
+            raise ParseError(s)
+        elif len(t) == 1: # only label
             if t[0][0] == ':':
-                self.llabel.append(t[0])
+                self.blabel_l.append(t[0])
             else:
-                raise Exception
-        elif len(t) == 2 and t[0][0] == '.':
-            self.dlabel.append(t[0], int(t[1], 0))
-        else:
+                raise ParseError(s)
+        elif len(t) == 2 and t[0][0] == '.': # immediate label
+            self.ilabel_al.append(t[0], int(t[1], 0))
+        else: # instruction
             if t[0][0] == ':':
-                self.llabel.append(t[0])
+                self.blabel_l.append(t[0])
                 self.opcode = t[1]
-                self.operand = t[2:]
+                self.operand_l = t[2:]
             else:
                 self.opcode = t[0]
-                self.operand = t[1:]
+                self.operand_l = t[1:]
 
-    def append(self, i):
+    def append(self, i): #append to line without instruction
         if (self.opcode == None
-            or self.operand == None):
+            and self.operand_l == None):
             raise Exception
-        self.llabel.extend(i.llabel)
-        self.dlabel.extend(i.dlabel)
+        self.blabel_l.extend(i.blabel_l)
+        self.ilabel_al.extend(i.ilabel_al)
         self.opcode = i.opcode
-        self.operand = i.operand
+        self.operand_l = i.operand_l
 
-    def conv(self, dlabs, llabs):
+    def conv_pseudo(self, ilabel_d):
+        def logical_rshift(x, n):
+            return (x % 0x100000000) >> n
+
+        if self.opcode == 'LDI':
+            if self.operand_l[1] == '.':
+                imm = ilabel_d[self.operand_l[1]]
+            else:
+                imm = int(self.operand_l[1], 0)
+            i1 = Instruction('LDIL ' + self.operand_l[0] + ' ' + str(imm & 2**20-1))
+            i1.blabel_l = self.blabel_l
+            higher_bits = logical_rshift(imm,20)
+            if higher_bits != 0:
+                i2 = Instruction('LDIH ' + self.operand_l[0] + ' ' + str(higher_bits))
+                return [i1, i2]
+            else:
+                return [i1]
+        else:
+            return [self]
+
+
+    def conv_bin(self, ilabel_d, blabel_d):
         opcode_dic = {'LD'  :0x0,
                       'ST'  :0x1,
                       'LDI' :0x2,
                       'STA' :0x3,
                       'LDIH':0x4,
-                      'LDIH':0x5,
+                      'LDIL':0x5,
                       'ADD' :0x6,
                       'SUB' :0x7,
                       'FNEG':0x8,
@@ -62,19 +91,21 @@ class Instruction:
                       'FADD':0x19,
                       'FMUL':0x1a}
         def reg2int(r):
-            if r.startwith('r'):
+            if r.startswith('r'):
                 return int(r.strip('r'), 0)
             else:
-                raise Exception
+                raise ParseError(r)
 
         def imm2int(r, bit):
-            n = int(r, 0)
-            if - 2**(bit-1) <= n and n < 2**(bit-1):
-                return n
+            if r[0] == '.': # immediate label
+                n = ilabel_d[r]
+            elif r[0] == ':': # branch label
+                n = blabel_d[r] - self.line
             else:
-                raise Exception
+                n = int(r, 0)
+            return n & (2**bit-1)
 
-        self.binary = opcode_dic(self.opcode) << 26
+        self.binary = opcode_dic[self.opcode] << 26
         if (self.opcode == 'LD' or
             self.opcode == 'ST' or
             self.opcode == 'ADDI' or
@@ -84,15 +115,15 @@ class Instruction:
             self.opcode == 'BLE' or
             self.opcode == 'BLT' or
             self.opcode == 'BFLE'):
-            self.binary |= (reg2int(self.operand[0]) & 2**6-1) << 20
-            self.binary |= (reg2int(self.operand[1]) & 2**6-1) << 14
-            self.binary |= imm2int(self.operand[2], 14)
+            self.binary |= (reg2int(self.operand_l[0]) & 2**6-1) << 20
+            self.binary |= (reg2int(self.operand_l[1]) & 2**6-1) << 14
+            self.binary |= imm2int(self.operand_l[2], 14)
         elif (self.opcode == 'LDA' or
               self.opcode == 'STA' or
               self.opcode == 'LDIH' or
               self.opcode == 'LDIL'):
-            self.binary |= (reg2int(self.operand[0]) & 2**6-1) << 20
-            self.binary |= (imm2int(self.operand[1]) & 2**6-1)
+            self.binary |= (reg2int(self.operand_l[0]) & 2**6-1) << 20
+            self.binary |= (imm2int(self.operand_l[1], 20) & 2**20-1)
         elif (self.opcode == 'ADD' or
               self.opcode == 'SUB' or
               self.opcode == 'AND' or
@@ -102,43 +133,85 @@ class Instruction:
               self.opcode == 'SHR' or
               self.opcode == 'FADD' or
               self.opcode == 'FMUL'):
-            self.binary |= (reg2int(self.operand[0]) & 2**6-1) << 20
-            self.binary |= (reg2int(self.operand[1]) & 2**6-1) << 14
-            self.binary |= (reg2int(self.operand[2]) & 2**6-1) << 8
+            self.binary |= (reg2int(self.operand_l[0]) & 2**6-1) << 20
+            self.binary |= (reg2int(self.operand_l[1]) & 2**6-1) << 14
+            self.binary |= (reg2int(self.operand_l[2]) & 2**6-1) << 8
         elif (self.opcode == 'FNEG'):
-            self.binary |= (reg2int(self.operand[0]) & 2**6-1) << 20
-            self.binary |= (reg2int(self.operand[1]) & 2**6-1) << 14
+            self.binary |= (reg2int(self.operand_l[0]) & 2**6-1) << 20
+            self.binary |= (reg2int(self.operand_l[1]) & 2**6-1) << 14
         elif (self.opcode == 'JSUB'):
-            self.binary |= (imm2int(self.operand[0]))
+            self.binary |= (imm2int(self.operand_l[0], 26))
         elif (self.opcode == 'RET'):
             pass
         elif (self.opcode == 'PUSH' or
               self.opcode == 'POP'):
-            self.opcode |= (reg2int(self.operand[0]) & 2**6-1) << 20;
+            self.binary |= (reg2int(self.operand_l[0]) & 2**6-1) << 20;
         else:
-            raise Exception
+            raise ParseError(self.opcode)
 
 
 class Assembly:
-    def __init__(self, assembly):
-        self.ins = []
-        self.llabs = dict()
-        self.dlabs = dict()
-        f = open(assembly, 'r')
+    import sys
+    def __init__(self, fin):
+        self.ins_l = [] # list of instructions
+        self.blabel_d = dict() # dictionary of branch label
+        self.ilabel_d = dict() # dictionary of immediate label
         while(True):
-            s = f.readline();
-            if s == '':
+            s = fin.readline();
+            if s.strip() == '':
                 break
             i = Instruction(s)
             while i.opcode == None:
-                i.append(Instruction(f.readline()));
-            self.ins.append(i)
-            for l,n in self.ins.dlabel:
-                self.dlabs[l] = n
+                i.append(Instruction(fin.readline()));
 
-        for n in range(len(self.ins)):
-            for l in self.ins[n].llabel:
-                self.llabs[l] = n
+            self.ins_l.append(i)
+            for i in self.ins_l:
+                for l, n in i.ilabel_al:
+                    self.ilabel_d[l] = n
 
-        for i in self.ins:
-            self.conv(self.llabs)
+        tmp = []
+        for i in self.ins_l:
+            tmp.extend(i.conv_pseudo(self.ilabel_d))
+        self.ins_l = tmp
+
+        for n,i in enumerate(self.ins_l):
+            i.line = n
+            for l in i.blabel_l:
+                self.blabel_d[l] = n
+
+        for i in self.ins_l:
+            i.conv_bin(self.ilabel_d, self.blabel_d)
+
+    def print_bin(self,fout=sys.stdout):
+        import struct
+        for i in self.ins_l:
+            fout.write(struct.pack('i', i.binary))
+
+    def print_text(self,fout=sys.stdout):
+        def print_byte(n):
+            fout.write(bin(tmp)[2:].zfill(8))
+
+        for i in self.ins_l:
+            tmp = (i.binary >> 24) & 255
+            print_byte(tmp)
+            tmp = (i.binary >> 16) & 255
+            print_byte(tmp)
+            tmp = (i.binary >> 8) & 255
+            print_byte(tmp)
+            tmp = i.binary & 255
+            print_byte(tmp)
+            fout.write('\n')
+
+    def show_immediate_label(self, fout=sys.stdout):
+        for l, n in self.ilabel_d.iteritems():
+            fout.write(l+' '+hex(n)+'\n')
+
+    def show_branch_label(self, fout=sys.stdout):
+        for l, n in self.blabel_d.iteritems():
+            fout.write(l+' '+hex(n)+'\n')
+
+
+fin = open("fib.s", 'r')
+a = Assembly(fin)
+a.print_text()
+a.show_branch_label()
--- core/file/fib.s
+++ core/file/fib.s
@@ -1,11 +1,14 @@
-ADDI	r1	r0	1 
-ADDI	r2	r0	1
-ADDI	r4	r0	0
-ADDI	r5	r0	9
-BEQ	r4	r5	6	#loop
-ADD	r3	r2	r1
-ADDI	r1	r2	0
-ADDI	r2	r3	0
-ADDI	r4	r4	1
-BEQ	r0	r0	-5
-ST	r2	r0	0xffff
+:main
+	ADDI	r1	r0	1	# r1 = 1 = f(x-1)
+	ADDI	r2	r0	1	# r2 = 1 = f(x)
+	ADDI	r4	r0	0	# r4 = 0 = x
+	ADDI	r5	r0	9	# r5 = 9
+:loop
+	BEQ	r4	r5	:end	# if x == 9
+	ADD	r3	r2	r1	# r3 = f(x+1) = f(x) + f(x-1)
+	ADDI	r1	r2	0	# r1 = f(x)
+	ADDI	r2	r3	0	# r2 = f(x+1)
+	ADDI	r4	r4	1	# r4 = x = x+1
+	BEQ	r0	r0	:loop	# loop
+:end
+	ST	r2	r0	0xffff
