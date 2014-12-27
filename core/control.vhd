c4d584d54f80238bdb9125450f574e47107807d4
Kamesawa Ryosuke <emak.ryo@gmail.com>
2014-12-27 21:52:58 +0900

    fix bug

--- core/control.vhd
+++ core/control.vhd
@@ -75,142 +75,151 @@ begin
     variable data_hazard : std_logic;
     variable mem_stall : std_logic;
   begin
-    v := r;
+    if rising_edge(clk) then
+      v := r;
 
-    op := inst(31 downto 26);
-    ra := unsigned(inst(25 downto 20));
-    rb := unsigned(inst(19 downto 14));
-    rc := unsigned(inst(13 downto 8));
+      op := inst(31 downto 26);
+      ra := unsigned(inst(25 downto 20));
+      rb := unsigned(inst(19 downto 14));
+      rc := unsigned(inst(13 downto 8));
 
-    --detect data hazard
-    --case op is
-    --  when ST =>
-    --    data_hazard := detect_data_hazard(ra, v) or
-    --                   detect_data_hazard(rb, v);
-    --  when others =>
-    --    data_hazard := '0';
-    --end case;
-    data_hazard := '0';
+      --detect data hazard
+      --case op is
+      --  when ST =>
+      --    data_hazard := detect_data_hazard(ra, v) or
+      --                   detect_data_hazard(rb, v);
+      --  when others =>
+      --    data_hazard := '0';
+      --end case;
+      data_hazard := '0';
 
+      --ready
+      if setup = 3 then
+        --stall for memory access
+        if r.mem.go = '0' and memo.busy = '0' then
 
-    --stall for memory access
-    if r.mem.go = '0' and memo.busy = '0' then
-
-      --fetch
-      if data_hazard = '0' and r.e.branch = '0' then
-        r.f.pc <= r.f.pc+1;
-      elsif data_hazard = '0' and r.e.branch = '1' then
-        r.f.pc <= alu_o.d(ADDR_WIDTH-1 downto 0);
-      end if;
-
-
-      --decode
-      if data_hazard = '0' and r.e.branch = '0' then
-        r.d.pc <= r.f.pc;
-        r.d.op <= op;
-        case op is
-          when ST =>
-            r.d.dest <= (others => '0');
-            r.d.data <= unsigned(resize(
-              signed(inst(13 downto 0)), 32));
-            r.d.reg.a1 <= ra;
-            r.d.reg.a2 <= rb;
-          when ADD =>
-            r.d.dest <= ra;
-            r.d.data <= (others => '0');
-            r.d.reg.a1 <= rb;
-            r.d.reg.a2 <= rc;
-          when ADDI =>
-            r.d.dest <= ra;
-            r.d.data <= unsigned(resize(
-              signed(inst(13 downto 0)), 32));
-            r.d.reg.a1 <= rb;
-            r.d.reg.a2 <= (others => '0');
-          when BEQ =>
-            r.d.dest <= (others => '0');
-            r.d.data <= unsigned(resize(
-              signed(inst(13 downto 0)), 32));
-            r.d.reg.a1 <= ra;
-            r.d.reg.a2 <= rb;
-          when others =>
-            r.d <= default_d;
-        end case;
-      elsif data_hazard = '0' and r.e.branch = '1' then
-        r.d <= default_d;
-      end if;
-
-
-      --execute
-      r.e.op <= r.d.op;
-      r.e.dest <= r.d.dest;
-      case r.d.op is
-        when ST =>
-          r.e.alu.d1 <= reg_o.d2;
-          r.e.alu.d2 <= r.d.data;
-          r.e.alu.ctrl <= "000";
-          r.e.branch <= '0';
-          r.e.data <= reg_o.d1;
-        when ADD =>
-          r.e.alu.d1 <= reg_o.d1;
-          r.e.alu.d2 <= reg_o.d2;
-          r.e.alu.ctrl <= "000";
-          r.e.branch <= '0';
-        when ADDI =>
-          r.e.alu.d1 <= reg_o.d1;
-          r.e.alu.d2 <= r.d.data;
-          r.e.alu.ctrl <= "000";
-          r.e.branch <= '0';
-        when BEQ =>
-          r.e.alu.d1 <= resize(r.d.pc, 32);
-          r.e.alu.d2 <= r.d.data;
-          if reg_o.d1 = reg_o.d2 then
-            r.e.branch <= '1';
-          else
-            r.e.branch <= '0';
+          --fetch
+          if r.e.branch = '0' then
+            r.f.pc <= r.f.pc+1;
+          else -- elsif r.e.branch = '1' then
+            r.f.pc <= alu_o.d(ADDR_WIDTH-1 downto 0);
           end if;
-        when others =>
-          r.e <= default_e;
-      end case;
 
 
-      --memory access
-      r.m.op <= r.e.op;
-      r.m.dest <= r.e.dest;
-      case r.e.op is
-        when ST =>
-          r.mem.a <= alu_o.d(19 downto 0);
-          r.mem.d <= r.e.data;
-          r.mem.go <= '1';
-          r.mem.we <= '1';
-        when ADD =>
-          r.m.data <= alu_o.d;
+          --decode
+          if r.e.branch = '0' then
+            r.d.pc <= r.f.pc;
+            r.d.op <= op;
+            case op is
+              when ST =>
+                r.d.dest <= (others => '0');
+                r.d.data <= unsigned(resize(
+                  signed(inst(13 downto 0)), 32));
+                r.d.reg.a1 <= ra;
+                r.d.reg.a2 <= rb;
+              when ADD =>
+                r.d.dest <= ra;
+                r.d.data <= (others => '0');
+                r.d.reg.a1 <= rb;
+                r.d.reg.a2 <= rc;
+              when ADDI =>
+                r.d.dest <= ra;
+                r.d.data <= unsigned(resize(
+                  signed(inst(13 downto 0)), 32));
+                r.d.reg.a1 <= rb;
+                r.d.reg.a2 <= (others => '0');
+              when BEQ =>
+                r.d.dest <= (others => '0');
+                r.d.data <= unsigned(resize(
+                  signed(inst(13 downto 0)), 32));
+                r.d.reg.a1 <= ra;
+                r.d.reg.a2 <= rb;
+              when others =>
+                r.d <= default_d;
+            end case;
+          else --elsif r.e.branch = '1' then
+            r.d <= default_d;
+          end if;
+
+
+          --execute
+          r.e.op <= r.d.op;
+          r.e.dest <= r.d.dest;
+          case r.d.op is
+            when ST =>
+              r.e.alu.d1 <= reg_o.d2;
+              r.e.alu.d2 <= r.d.data;
+              r.e.alu.ctrl <= "000";
+              r.e.branch <= '0';
+              r.e.data <= reg_o.d1;
+            when ADD =>
+              r.e.alu.d1 <= reg_o.d1;
+              r.e.alu.d2 <= reg_o.d2;
+              r.e.alu.ctrl <= "000";
+              r.e.branch <= '0';
+            when ADDI =>
+              r.e.alu.d1 <= reg_o.d1;
+              r.e.alu.d2 <= r.d.data;
+              r.e.alu.ctrl <= "000";
+              r.e.branch <= '0';
+            when BEQ =>
+              r.e.alu.d1 <= resize(r.d.pc, 32);
+              r.e.alu.d2 <= r.d.data;
+              if reg_o.d1 = reg_o.d2 then
+                r.e.branch <= '1';
+              else
+                r.e.branch <= '0';
+              end if;
+            when others =>
+              r.e <= default_e;
+          end case;
+
+
+          --memory access
+          r.m.op <= r.e.op;
+          r.m.dest <= r.e.dest;
+          case r.e.op is
+            when ST =>
+              r.mem.a <= alu_o.d(19 downto 0);
+              r.mem.d <= r.e.data;
+              r.mem.go <= '1';
+              r.mem.we <= '1';
+            when ADD =>
+              r.m.data <= alu_o.d;
+              r.mem <= default_mem_in;
+            when ADDI =>
+              r.m.data <= alu_o.d;
+              r.mem <= default_mem_in;
+            when BEQ =>
+              r.m.data <= alu_o.d;
+              r.mem <= default_mem_in;
+            when others =>
+              r.m <= default_m;
+              r.mem <= default_mem_in;
+          end case;
+
+
+          -- write
+          case r.m.op is
+            when ADD | ADDI =>
+              r.w.reg.we <= '1';
+              r.w.reg.a <= r.m.dest;
+              r.w.reg.d <= r.m.data;
+            when BEQ =>
+              r.w.reg.we <= '0';
+            when others =>
+              r.w.reg.we <= '0';
+          end case;
+
+        else
           r.mem <= default_mem_in;
-        when ADDI =>
-          r.m.data <= alu_o.d;
-          r.mem <= default_mem_in;
-        when BEQ =>
-          r.m.data <= alu_o.d;
-          r.mem <= default_mem_in;
-        when others =>
-          r.m <= default_m;
-          r.mem <= default_mem_in;
-      end case;
+        end if;
 
+      else --setup
+        r <= init_r;
+        setup <= setup+1;
+      end if;
 
-      -- write
-      case r.m.op is
-        when ADD | ADDI =>
-          r.w.reg.we <= '1';
-          r.w.reg.a <= r.m.dest;
-          r.w.reg.d <= r.m.data;
-        when BEQ =>
-          r.w.reg.we <= '0';
-        when others =>
-          r.w.reg.we <= '0';
-      end case;
-
-    else
-      r.mem <= default_mem_in;
     end if;
 
   end process;
