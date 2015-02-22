481f762ba5f642dc5cc8c2c74d83314b25296e6c
Osamu Ishimura <oishimura@live.jp>
2015-02-23 01:23:19 +0900

    Integrate fadd and fmul to simulator

--- simulator/fpu.h
+++ simulator/fpu.h
@@ -1,16 +1,17 @@
 #pragma once
 #include <iostream>
+#include <stdint.h>
 
 class FPU{
 	public:
-		static uint32_t fadd(uint32_t, uint32_t);
-		static uint32_t fsub(uint32_t, uint32_t);
-		static uint32_t fmul(uint32_t, uint32_t);
-		static uint32_t fdiv(uint32_t, uint32_t);
-		static int fcmp(uint32_t, uint32_t);
-		static uint32_t fsqrt(uint32_t);
-		static uint32_t fabs(uint32_t);
-		static uint32_t ftoi(uint32_t);
-		static uint32_t itof(uint32_t);
+		static uint32_t add(uint32_t, uint32_t);
+		static uint32_t sub(uint32_t, uint32_t);
+		static uint32_t mul(uint32_t, uint32_t);
+		static uint32_t div(uint32_t, uint32_t);
+		static int cmp(uint32_t, uint32_t);
+		static uint32_t sqrt(uint32_t);
+		static uint32_t abs(uint32_t);
+		static uint32_t _ftoi(uint32_t);
+		static uint32_t _itof(uint32_t);
 
 };
--- simulator/fpu_n.cpp
+++ simulator/fpu_n.cpp
@@ -7,35 +7,35 @@ typedef union{
 	float f;
 }FU;
 
-uint32_t FPU::fadd(uint32_t x, uint32_t y){
+uint32_t FPU::add(uint32_t x, uint32_t y){
 	FU xfu, yfu, zfu;
 	xfu.r = x; yfu.r = y;
 	zfu.f = xfu.f + yfu.f;
 	return zfu.r;
 }
 
-uint32_t FPU::fsub(uint32_t x, uint32_t y){
+uint32_t FPU::sub(uint32_t x, uint32_t y){
 	FU xfu, yfu, zfu;
 	xfu.r = x; yfu.r = y;
 	zfu.f = xfu.f - yfu.f;
 	return zfu.r;
 }
 
-uint32_t FPU::fmul(uint32_t x, uint32_t y){
+uint32_t FPU::mul(uint32_t x, uint32_t y){
 	FU xfu, yfu, zfu;
 	xfu.r = x; yfu.r = y;
 	zfu.f = xfu.f * yfu.f;
 	return zfu.r;
 }
 
-uint32_t FPU::fdiv(uint32_t x, uint32_t y){
+uint32_t FPU::div(uint32_t x, uint32_t y){
 	FU xfu, yfu, zfu;
 	xfu.r = x; yfu.r = y;
 	zfu.f = xfu.f / yfu.f;
 	return zfu.r;
 }
 
-int FPU::fcmp(uint32_t x, uint32_t y){
+int FPU::cmp(uint32_t x, uint32_t y){
 	FU xfu, yfu, zfu;
 	xfu.r = x; yfu.r = y;
 	zfu.f = xfu.f - yfu.f;
@@ -44,28 +44,28 @@ int FPU::fcmp(uint32_t x, uint32_t y){
 	else return 0;
 }
 
-uint32_t FPU::fsqrt(uint32_t x){
+uint32_t FPU::sqrt(uint32_t x){
 	FU xfu, yfu;
 	xfu.r = x;
 	yfu.f = std::sqrt(xfu.f);
 	return yfu.r;
 }
 
-uint32_t FPU::fabs(uint32_t x){
+uint32_t FPU::abs(uint32_t x){
 	FU xfu, yfu;
 	xfu.r = x;
 	yfu.f = std::abs(xfu.f);
 	return yfu.r;
 }
 
-uint32_t FPU::ftoi(uint32_t x){
+uint32_t FPU::_ftoi(uint32_t x){
 	FU xfu, yfu;
 	xfu.r = x;
 	yfu.d = xfu.f;
 	return yfu.r;
 }
 
-uint32_t FPU::itof(uint32_t x){
+uint32_t FPU::_itof(uint32_t x){
 	FU xfu, yfu;
 	xfu.r = x;
 	yfu.f = xfu.d;
