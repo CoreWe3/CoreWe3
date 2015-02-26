2c1c959380a91a6925a707f580fdf7f3988d903b
Ryosuke Kamesawa <emak.ryo@gmail.com>
2015-02-26 17:03:38 +0900

    expand instruction memory

--- core/file/convert01.py
+++ core/file/convert01.py
@@ -8,7 +8,7 @@ def convert01(instruction):
     return str01
 
 
-for i in range(2**12):
+for i in range(2**13):
     instruction = sys.stdin.read(4)
     if len(instruction) == 4:
         print convert01(instruction)
