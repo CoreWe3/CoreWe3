121bb2a9f537719622911bbf80a84e36696b83de
Ryosuke Kamesawa <emak.ryo@gmail.com>
2015-02-26 11:53:14 +0900

    fix bugs of pipeline and enable assembleer to read float immediate

--- core/debug/CoreTestBench.vhd
+++ core/debug/CoreTestBench.vhd
@@ -43,7 +43,7 @@ architecture CoreTestBench_arch of CoreTestBench is
   component InputSimulator
     generic (
       wtime : std_logic_vector(15 downto 0) := wtime;
-      INPUT_FILE : string := "../../file/test");
+      INPUT_FILE : string := "../../file/fib_loop");
     port (
       clk  : in  std_logic;
       RS_RX   : out std_logic);
