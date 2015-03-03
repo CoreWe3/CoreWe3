b10622b0fe30a4769c117a313ce11744a2baf476
wataru <wtr816@gmail.com>
2015-03-03 17:53:54 +0900

    libmincaml.S/boot.s

--- lib/asm/boot.s
+++ lib/asm/boot.s
@@ -1,8 +1,3 @@
-.min_caml_finv_table_a 0xEF000
-.min_caml_finv_table_b 0xEF400
-.min_caml_sqrt_table_a 0xEF800
-.min_caml_sqrt_table_b 0xEFC00
-	LDI	r2	489471	#init sp
+	LDI	r2	0xfdfff	#init sp
 	LDI	r1	0	#init hp
-	JSUB	:_min_caml_start
-	BEQ	r0	r0	0
+	J	:_min_caml_start
