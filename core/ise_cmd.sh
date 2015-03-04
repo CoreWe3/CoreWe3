113c8c92a0ed545ccd0dbf992613a38c1d147b8b
Ryosuke Kamesawa <emak.ryo@gmail.com>
2015-03-04 12:27:48 +0900

    modify synthesize scripts

--- core/ise_cmd.sh
+++ core/ise_cmd.sh
@@ -2,10 +2,14 @@
 LANGUAGE=
 XILINX_DIR=/opt/Xilinx/
 export XILINXD_LICENSE_FILE=2100@idylls.jp
+if [ ! -d ise ]; then mkdir ise; fi
+cd ise
+ls ../*.vhd ../../FPU/*.vhd | awk '{print "vhdl work " $0}' > core.prj
 . ${XILINX_DIR}14.4/ISE_DS/settings64.sh > /dev/null && \
-xst -intstyle ise -ifn core.xst && \
-ngdbuild -intstyle ise -dd _ngo -nt timestamp -uc core.ucf -p xc5vlx50t-ff1136-1 core.ngc core.ngd && \
+xst -intstyle ise -ifn ../core.xst && \
+ngdbuild -intstyle ise -dd _ngo -nt timestamp -uc ../core.ucf -p xc5vlx50t-ff1136-1 core.ngc core.ngd && \
 map -intstyle ise -p xc5vlx50t-ff1136-1 -w -logic_opt off -ol high -t 1 -register_duplication off -global_opt off -mt off -cm area -ir off -pr off -lc off -power off -o core_map.ncd core.ngd core.pcf && \
 par -w -intstyle ise -ol high -mt off core_map.ncd core.ncd core.pcf && \
 trce -intstyle ise -v 3 -s 1 -n 3 -fastpaths -xml core.twx core.ncd -o core.twr core.pcf && \
-bitgen -intstyle ise -f core.ut core.ncd
+bitgen -intstyle ise -f ../core.ut core.ncd
+cp core.bit ..
