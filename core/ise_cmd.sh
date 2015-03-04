c58bf6d2a49d7d64eed8bd7ff51e5547b784a1bb
Ryosuke Kamesawa <emak.ryo@gmail.com>
2015-03-04 20:12:25 +0900

    run mandelbrot.ml without diff

--- core/ise_cmd.sh
+++ core/ise_cmd.sh
@@ -11,5 +11,5 @@ ngdbuild -intstyle ise -dd _ngo -nt timestamp -uc ../core.ucf -p xc5vlx50t-ff113
 map -intstyle ise -p xc5vlx50t-ff1136-1 -w -logic_opt off -ol high -t 1 -register_duplication off -global_opt off -mt off -cm area -ir off -pr off -lc off -power off -o core_map.ncd core.ngd core.pcf && \
 par -w -intstyle ise -ol high -mt off core_map.ncd core.ncd core.pcf && \
 trce -intstyle ise -v 3 -s 1 -n 3 -fastpaths -xml core.twx core.ncd -o core.twr core.pcf && \
-bitgen -intstyle ise -f ../core.ut core.ncd
+bitgen -intstyle ise -f ../core.ut core.ncd && \
 cp core.bit ..
