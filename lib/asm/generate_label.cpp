dc0bc66b1251bbee4d3a6a012eee4ab197e179be
wataru <wtr816@gmail.com>
2015-03-08 18:45:02 +0900

    generate global_labels

---
+++ lib/asm/generate_label.cpp
@@ -0,0 +1,54 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+string globals[] = {
+    ".n_objects",
+    ".objects",
+    ".screen",
+    ".viewpoint",
+    ".light",
+    ".beam",
+    ".and_net",
+    ".or_net",
+    ".solver_dist",
+    ".intsec_rectside",
+    ".tmin",
+    ".intersection_point",
+    ".intersected_object_id",
+    ".nvector",
+    ".texture_color",
+    ".diffuse_ray",
+    ".rgb",
+    ".image_size",
+    ".image_center",
+    ".scan_pitch",
+    ".startp",
+    ".startp_fast",
+    ".screenx_dir",
+    ".screeny_dir",
+    ".screenz_dir",
+    ".ptrace_dirvec",
+    ".dirvecs",
+    ".light_dirvec",
+    ".reflections",
+    ".n_reflections"};
+
+int main(){
+    ifstream dump;
+    dump.open("init_globals.dump");
+    if(dump.fail()){
+	cerr << "Can't open file" << endl;
+	return 1;
+    }
+    
+    int tmp;
+    int i = 0;
+    while(dump.read(reinterpret_cast<char*>(&tmp), sizeof(tmp))){
+	cout << globals[i] << " " << tmp << endl;
+	i++;
+    }
+    return 0;
+}
