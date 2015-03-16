f921c1fe1aa76b998731b4e84e284b6f74b65f5a
Ryosuke Kamesawa <emak.ryo@gmail.com>
2015-03-16 19:17:36 +0900

    implement cache as distributed RAM run newton

--- core/MemoryCache.vhd
+++ core/MemoryCache.vhd
@@ -22,7 +22,7 @@ architecture MemoryCache_arch of MemoryCache is
   constant TAGW : integer := 20 - INDEXW - 2;
 
   type cache_t is array (2**INDEXW-1 downto 0) of
-    unsigned(31 downto 0);
+    unsigned(127 downto 0);
 
   type tag_t is array (2**INDEXW-1 downto 0) of
     unsigned(TAGW-1 downto 0);
@@ -40,13 +40,18 @@ architecture MemoryCache_arch of MemoryCache is
     data : unsigned(31 downto 0);
     we : std_logic; --write enable
     ZD : unsigned(31 downto 0);
-    busy : std_logic;
-    complete : unsigned(1 downto 0);
-    da : unsigned(127 downto 0);
-    db : unsigned(127 downto 0);
     wa : unsigned(3 downto 0);
     wb : unsigned(3 downto 0);
   end record;
+
+  constant initial_state : memory_cache_t := (
+    req => default_memory_request,
+    state => "0000",
+    data => (others => '-'),
+    we => '0',
+    ZD => (others => '-'),
+    wa => (others => '0'),
+    wb => (others => '0'));
 
   impure function init_tag(dummy : in std_logic) return tag_t is
     variable tag : tag_t;
@@ -66,35 +71,21 @@ architecture MemoryCache_arch of MemoryCache is
     return status;
   end function;
 
-  signal cache_a0 : cache_t;
-  signal cache_a1 : cache_t;
-  signal cache_a2 : cache_t;
-  signal cache_a3 : cache_t;
-  signal cache_b0 : cache_t;
-  signal cache_b1 : cache_t;
-  signal cache_b2 : cache_t;
-  signal cache_b3 : cache_t;
+  signal cache_a : cache_t;
+  signal cache_b : cache_t;
   signal taga : tag_t := init_tag('0');
   signal tagb : tag_t := init_tag('0');
   signal status : status_t := init_status('0');
-  signal r : memory_cache_t :=
-    (default_memory_request, "0000", (others => '-'), '0',
-     (others => '-'), '0', "00", (others => '-'), (others => '-'),
-     (others => '0'), (others => '0'));
+  signal r : memory_cache_t := initial_state;
 
 begin
   ZA <= std_logic_vector(request.a) when r.state = "0000" else
         std_logic_vector(r.req.a);
   XWA <= not request.we when r.state = "0000" else
-         not r.req.we;
+         '1';
   ZD <= std_logic_vector(r.data) when r.we = '1' else
         (others => 'Z');
-  ADVA <= '1' when r.state = "0010" else
-          '1' when r.state = "0011" else
-          '1' when r.state = "1000" else
-          '1' when r.state = "1001" else
-          '1' when r.state = "1100" else
-          '0';
+  ADVA <= '0';
 
   process(clk)
     variable v : memory_cache_t;
@@ -103,15 +94,17 @@ begin
     variable tag : unsigned(TAGW-1 downto 0);
     variable vtaga, vtagb : unsigned(TAGW-1 downto 0);
     variable vstatus : unsigned(1 downto 0);
-    variable da, db : unsigned(31 downto 0);
-    variable wsel : unsigned(3 downto 0);
+    variable da, db, rda, rdb : unsigned(31 downto 0);
+    variable oda, odb, ida, idb : unsigned(127 downto 0);
+    variable vreply : memory_reply_t;
+    variable sel : unsigned(3 downto 0);
   begin
     if rising_edge(clk) then
       v := r;
       v.data := (others => '-');
       v.we := '0';
       v.ZD := unsigned(ZD);
-      v.complete := "00";
+      vreply := ((others => '-'), '0', '0');
 
       if r.state = "0000" then
         v.req := request;
@@ -124,12 +117,26 @@ begin
       vtaga := taga(to_integer(index));
       vtagb := tagb(to_integer(index));
       vstatus := status(to_integer(index));
+      oda := cache_a(to_integer(index));
+      odb := cache_b(to_integer(index));
 
       case baddr is
-        when "00" => wsel := "0001";
-        when "01" => wsel := "0010";
-        when "10" => wsel := "0100";
-        when others => wsel := "1000";
+        when "00" =>
+          sel := "0001";
+          rda := oda(31 downto 0);
+          rdb := odb(31 downto 0);
+        when "01" =>
+          sel := "0010";
+          rda := oda(63 downto 32);
+          rdb := odb(63 downto 32);
+        when "10" =>
+          sel := "0100";
+          rda := oda(95 downto 64);
+          rdb := odb(95 downto 64);
+        when others =>
+          sel := "1000";
+          rda := oda(127 downto 96);
+          rdb := odb(127 downto 96);
       end case;
 
       case r.state is
@@ -140,54 +147,72 @@ begin
             v.req.a(19 downto ADDR_WIDTH) /= ones(19 downto ADDR_WIDTH) then
             if v.req.we = '1' and vstatus /= "00" and vtaga = tag then
               --store cache_a
+              v.wa := sel;
               da := v.req.d;
-              v.wa := wsel;
               if vstatus = "10" then
                 vstatus := "11";
               end if;
             elsif v.req.we = '1' and vstatus(1) = '1' and vtagb = tag then
               -- store cache_b
+              v.wb := sel;
               db := v.req.d;
-              v.wb := wsel;
               vstatus := "10";
             elsif v.req.we = '1' and vstatus(0) = '0' then
               -- store cache_a
               vtaga := tag;
+              v.wa := sel;
               da := v.req.d;
-              v.wa := wsel;
+              vreply.busy := '1';
               if vstatus(1) = '0' then
                 vstatus := "01";
               else
                 vstatus := "11";
               end if;
-              v.req.we := '0';
-              v.req.a := tag & index & next_baddr;
               v.state := "0001";
+              v.req.a := tag & index & next_baddr;
             elsif v.req.we = '1' then
               -- store cache_b
               vtagb := tag;
+              v.wb := sel;
               db := v.req.d;
-              v.wb := wsel;
+              vreply.busy := '1';
               vstatus := "10";
-              v.req.we := '0';
-              v.req.a := tag & index & next_baddr;
               v.state := "0001";
+              v.req.a := tag & index & next_baddr;
 
             elsif v.req.we = '0' and vstatus /= "00" and
               vtaga = tag then
               -- load cache_a
-              v.complete := "01";
+              vreply.d := rda;
+              vreply.complete := '1';
               if vstatus = "10" then
                 vstatus := "11";
               end if;
             elsif v.req.we = '0' and vstatus(1) = '1' and
               vtagb = tag then
               -- load cache_b
-              v.complete := "10";
+              vreply.d := rdb;
+              vreply.complete := '1';
             elsif v.req.we = '0' then
               -- load SRAM
-              v.busy := '1';
-              v.state := "1000";
+              case vstatus is
+                when "00" =>
+                  vtaga := tag;
+                  vstatus := "01";
+                  v.wa := sel;
+                when "01" | "11" =>
+                  vtagb := tag;
+                  vstatus := "10";
+                  v.wb := sel;
+                when "10" =>
+                  vtaga := tag;
+                  vstatus := "11";
+                  v.wa := sel;
+                when others =>
+              end case;
+              vreply.busy := '1';
+              v.state := "1001";
+              v.req.a := tag & index & next_baddr;
             end if;
           end if;
 
@@ -198,104 +223,105 @@ begin
           end if;
 
         when "0001" => -- store miss
-          v.state := "0010";
           v.data := r.req.d;
           v.we := '1';
+          v.req.a := tag & index & next_baddr;
+          vreply.busy := '1';
+          v.state := "0010";
         when "0010" =>
+          v.req.a := tag & index & next_baddr;
+          vreply.busy := '1';
           v.state := "0011";
         when "0011" =>
+          vreply.busy := '1';
           v.state := "0100";
         when "0100" =>
+          v.wa := v.wa(2 downto 0) & v.wa(3);
+          da := r.ZD;
+          v.wb := v.wb(2 downto 0) & v.wb(3);
+          db := r.ZD;
+          vreply.busy := '1';
           v.state := "0101";
-          v.wa := v.wa(2 downto 0) & v.wa(3);
-          da := r.ZD;
-          v.wb := v.wb(2 downto 0) & v.wb(3);
-          db := r.ZD;
         when "0101" =>
+          v.wa := v.wa(2 downto 0) & v.wa(3);
+          da := r.ZD;
+          v.wb := v.wb(2 downto 0) & v.wb(3);
+          db := r.ZD;
+          vreply.busy := '1';
           v.state := "0110";
-          v.wa := v.wa(2 downto 0) & v.wa(3);
-          da := r.ZD;
-          v.wb := v.wb(2 downto 0) & v.wb(3);
-          db := r.ZD;
         when "0110" =>
-          v.state := "0000";
           v.wa := v.wa(2 downto 0) & v.wa(3);
           da := r.ZD;
           v.wb := v.wb(2 downto 0) & v.wb(3);
           db := r.ZD;
+          v.state := "0000";
 
-        when "1000" => -- load miss
-          v.state := "1001";
-          case vstatus is
-            when "00" =>
-              vtaga := tag;
-              vstatus := "01";
-              v.wa := wsel;
-            when "01" | "11" =>
-              vtagb := tag;
-              vstatus := "10";
-              v.wb := wsel;
-            when "10" =>
-              vtaga := tag;
-              vstatus := "11";
-              v.wa := wsel;
-            when others =>
-          end case;
-        when "1001" =>
+        when "1001" => -- load miss
+          v.req.a := tag & index & next_baddr;
+          vreply.busy := '1';
+          v.state := "1010";
+        when "1010" =>
+          v.req.a := tag & index & next_baddr;
+          vreply.busy := '1';
           v.state := "1100";
         when "1100" =>
+          da := r.ZD;
+          db := r.ZD;
+          vreply.d := r.ZD;
+          vreply.busy := '1';
+          vreply.complete := '1';
           v.state := "1101";
-          da := r.ZD;
-          db := r.ZD;
-          v.data := r.ZD;
-          v.complete := "11";
         when "1101" =>
+          da := r.ZD;
+          db := r.ZD;
+          v.wa := v.wa(2 downto 0) & v.wa(3);
+          v.wb := v.wb(2 downto 0) & v.wb(3);
           v.state := "1110";
-          da := r.ZD;
-          v.wa := v.wa(2 downto 0) & v.wa(3);
-          db := r.ZD;
-          v.wb := v.wb(2 downto 0) & v.wb(3);
+          vreply.busy := '1';
         when "1110" =>
+          da := r.ZD;
+          db := r.ZD;
+          v.wa := v.wa(2 downto 0) & v.wa(3);
+          v.wb := v.wb(2 downto 0) & v.wb(3);
           v.state := "1111";
-          da := r.ZD;
-          v.wa := v.wa(2 downto 0) & v.wa(3);
-          db := r.ZD;
-          v.wb := v.wb(2 downto 0) & v.wb(3);
+          vreply.busy := '1';
         when "1111" =>
-          v.state := "0000";
           da := r.ZD;
-          v.wa := v.wa(2 downto 0) & v.wa(3);
           db := r.ZD;
+          v.wa := v.wa(2 downto 0) & v.wa(3);
           v.wb := v.wb(2 downto 0) & v.wb(3);
+          v.state := "0000";
         when others =>
           v.state := "0000";
       end case;
 
+      ida := oda;
+      idb := odb;
       if r.state = "0000" or r.state(2) = '1' then
         if v.wa(0) = '1' then
-          cache_a0(to_integer(index)) <= da;
+          ida(31 downto 0) := da;
         end if;
         if v.wa(1) = '1' then
-          cache_a1(to_integer(index)) <= da;
+          ida(63 downto 32) := da;
         end if;
         if v.wa(2) = '1' then
-          cache_a2(to_integer(index)) <= da;
+          ida(95 downto 64) := da;
         end if;
         if v.wa(3) = '1' then
-          cache_a3(to_integer(index)) <= da;
+          ida(127 downto 96) := da;
         end if;
 
         if v.wb(0) = '1' then
-          cache_b0(to_integer(index)) <= db;
+          idb(31 downto 0) := db;
         end if;
         if v.wb(1) = '1' then
-          cache_b1(to_integer(index)) <= db;
+          idb(63 downto 32) := db;
         end if;
         if v.wb(2) = '1' then
-          cache_b2(to_integer(index)) <= db;
+          idb(95 downto 64) := db;
         end if;
         if v.wb(3) = '1' then
-          cache_b3(to_integer(index)) <= db;
+          idb(127 downto 96) := db;
         end if;
       end if;
 
@@ -303,33 +329,13 @@ begin
       taga(to_integer(index)) <= vtaga;
       tagb(to_integer(index)) <= vtagb;
       status(to_integer(index)) <= vstatus;
+      cache_a(to_integer(index)) <= ida;
+      cache_b(to_integer(index)) <= idb;
 
       r <= v;
-      r.da <= cache_a0(to_integer(index)) &
-              cache_a1(to_integer(index)) &
-              cache_a2(to_integer(index)) &
-              cache_a3(to_integer(index));
-      r.db <= cache_b0(to_integer(index)) &
-              cache_b1(to_integer(index)) &
-              cache_b2(to_integer(index)) &
-              cache_b3(to_integer(index));
+      reply <= vreply;
 
     end if;
   end process;
-
-  reply.busy <= '0' when r.state = "0000" else
-                '1';
-  reply.complete <= '0' when r.complete = "00" else
-                    '1';
-  reply.d <= r.data when r.complete = "11" else
-             r.da(127 downto 96) when r.complete = "01" and r.req.a(1 downto 0) = "00" else
-             r.da(95 downto 64) when r.complete = "01" and r.req.a(1 downto 0) = "01" else
-             r.da(63 downto 32) when r.complete = "01" and r.req.a(1 downto 0) = "10" else
-             r.da(31 downto 0) when r.complete = "01" and r.req.a(1 downto 0) = "11" else
-             r.db(127 downto 96) when r.complete = "10" and r.req.a(1 downto 0) = "00" else
-             r.db(95 downto 64) when r.complete = "10" and r.req.a(1 downto 0) = "01" else
-             r.db(63 downto 32) when r.complete = "10" and r.req.a(1 downto 0) = "10" else
-             r.db(31 downto 0) when r.complete = "10" and r.req.a(1 downto 0) = "11" else
-             (others => '-');
 
 end MemoryCache_arch;
